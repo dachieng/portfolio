ece26be143d0ef4f1fd494028733a6b78cafaa03
dachieng <oloodorcas99@gmail.com>
2025-06-17 14:26:21 +0300

    feat: adjust titles

--- src/modules/helpers/index.ts
+++ src/modules/helpers/index.ts
@@ -1,6 +1,6 @@
 export const experiences = [
   {
-    title: 'Frontend Developer',
+    title: 'Software Developer',
     company: 'Instadiver',
     period: 'February 2024 - Present',
     responsibilities: [
@@ -11,7 +11,7 @@ export const experiences = [
     ],
   },
   {
-    title: 'Frontend Developer',
+    title: 'Software Developer',
     company: 'Qawqal',
     period: 'August 2024 - Present',
     responsibilities: [
